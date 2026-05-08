92886b7ade22c8838e2700e0e822e14c9c1bc280
adbr23 <adele.brunstad@gmail.com>
2026-05-08 08:20:35 +0200

    endringer2

---
+++ app.js
@@ -0,0 +1,32 @@
+const moreEl = document.querySelector(".more-tekst")
+const aboutBtnEls = document.querySelectorAll(".bokbtn") /* finner alle knappene */
+
+
+
+
+aboutBtnEls.forEach(function (knapp) {           /* looper gjennom hver knapp */
+    knapp.addEventListener("click", function () {
+        const moreTekst = knapp.parentElement.querySelector(".more-tekst")  /* finner .more-tekst som ligger i samme .bok-tekst som knappen */
+
+        moreTekst.classList.toggle("active")
+
+        if (moreTekst.classList.contains("active")) {
+            knapp.textContent = "Se mindre"
+        } else {
+            knapp.textContent = "Se mer"
+        }
+
+
+    })
+})
+
+
+
+const starEls = document.querySelectorAll(".fa-star")
+
+for (const starEl of starEls) {
+    starEl.addEventListener("click", function () {
+        starEl.classList.toggle("active")
+    })
+}
+
