7308dd00a188479ef45e1a81dc0e41fc408bad31
adbr23 <adele.brunstad@gmail.com>
2026-06-01 08:37:08 +0200

    litt endring

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const ukensItemsEls = document.querySelectorAll(".ukens-card-item")
 for (const ukensItemEl of ukensItemsEls) {
     ukensItemEl.addEventListener("click", function(){
         ukensItemEl.classList.toggle("active")
+        
     })
     
 }
